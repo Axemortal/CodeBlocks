9b5c59f6ab1ec3582ecb74e01f112bcd27035414
Wu Yifan <94105227+Axemortal@users.noreply.github.com>
2024-02-01 15:18:50 +0800

    Add 404 page

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,14 +1,24 @@
 import { Routes } from '@angular/router';
-import { LayoutComponent } from './modules/home/layout/layout.component';
+import { LayoutComponent as HomeLayoutComponent } from './modules/home/layout/layout.component';
+import { LayoutComponent as NotFoundLayoutComponent } from './modules/not-found/layout/layout.component';
 
 export const routes: Routes = [
   {
     path: 'home',
-    component: LayoutComponent,
+    component: HomeLayoutComponent,
+    pathMatch: 'full',
     loadChildren: () =>
       import('./modules/home/home.module').then((m) => m.HomeModule),
   },
-  { path: '404', component: LayoutComponent },
+  {
+    path: '404',
+    component: NotFoundLayoutComponent,
+    pathMatch: 'full',
+    loadChildren: () =>
+      import('./modules/not-found/not-found.module').then(
+        (m) => m.NotFoundModule
+      ),
+  },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: '404' },
 ];
