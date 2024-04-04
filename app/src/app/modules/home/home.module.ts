7d930e167e5b1c89f4d3439a4fce2c2cc7be41d8
clairecxy <claire.chu.ccxy@gmail.com>
2024-04-05 00:01:04 +0800

    cleaned some parts of layout code

--- app/src/app/modules/home/home.module.ts
+++ app/src/app/modules/home/home.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { CaptureComponent } from './capture/capture.component';
-import { LandingComponent } from './landing/landing.component';
 import { BlocklyComponent } from './blockly/blockly.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
@@ -10,7 +9,6 @@ import { WelcomeComponent } from './welcome/welcome.component';
   declarations: [
     LayoutComponent,
     CaptureComponent,
-    LandingComponent,
     BlocklyComponent,
     WelcomeComponent,
   ],
