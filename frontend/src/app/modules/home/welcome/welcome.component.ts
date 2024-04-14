ecd8d4cfa6f3b5cf215f96585512bd42f48b7d4f
clairecxy <claire.chu.ccxy@gmail.com>
2024-04-14 22:49:16 +0800

    Add instructions for website

--- frontend/src/app/modules/home/welcome/welcome.component.ts
+++ frontend/src/app/modules/home/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './welcome.component.scss'
 })
 export class WelcomeComponent {
-
+  onInstructions = false;
+  
+  goToInstructions(){
+    this.onInstructions = true;
+  }
 }
