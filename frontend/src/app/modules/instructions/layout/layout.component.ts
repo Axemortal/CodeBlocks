65094586003efe7500c8f9fc9e023a0dcd032aef
clairecxy <claire.chu.ccxy@gmail.com>
2024-04-19 00:19:31 +0800

    added instructions UI

--- frontend/src/app/modules/instructions/layout/layout.component.ts
+++ frontend/src/app/modules/instructions/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
-
+  constructor(private router: Router){}
+  
+  goToHome(){
+    console.log("y")
+    this.router.navigate(['/home']);
+  }
 }
